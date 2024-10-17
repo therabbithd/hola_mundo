54e08902c1c0f6d71efe4aa25bb623bd5d7f65d2
therabbithd <65951194+therabbithd@users.noreply.github.com>
2024-10-17 14:37:05 +0200

    scanner

--- hello_word.java
+++ hello_word.java
@@ -13,11 +13,14 @@ public class hello_word {
     }
 
     public static void saludo() {
+<<<<<<< HEAD
+     System.out.println("hola")   
         Scanner scanner = new Scanner(System.in);
         System.out.println("pon tu nombre");
         String nombre =  scanner.nextLine();
         System.out.println("hola "+ nombre);   
         scanner.close();
+>>>>>>> 1d797c16a1c1520700fac023df9293a5879dbaa2
     }
 
     public static void despedida() {
