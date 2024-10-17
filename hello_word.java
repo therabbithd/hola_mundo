ee1ea91297249219c93100322b6d85d02d64fe4b
therabbithd <65951194+therabbithd@users.noreply.github.com>
2024-10-17 14:09:40 +0200

    holamundo

---
+++ hello_word.java
@@ -0,0 +1,23 @@
+/**
+ * HolaMundo
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static void holaMundo() {
+        
+    }
+
+    public static void saludo() {
+        
+    }
+
+    public static void despedida() {
+        
+    }
+
+
+}
