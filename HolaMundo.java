96317858855e8c4701ba268fb9f4733164193141
Paco-89 <flyingv89@gmail.com>
2024-10-17 14:19:45 +0200

    Update HolaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,25 @@
+/**
+ * HolaMundo
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        holaMundo();
+        saludo();
+        despedida();
+    }
+
+    public static void holaMundo() {
+        System.out.println("hola caracola");
+    }
+
+    public static void saludo() {
+        
+    }
+
+    public static void despedida() {
+        
+    }
+
+
+}
